f190bb02202a5d6ee74f9c4cc2507db4a5282dc2
Ashan Wickramanayaka <wmashan2@gmail.com>
2025-07-31 21:42:01 +0530

    category page

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import UploadCategoryModel from '../components/UploadCategoryModel'
+import Loading from '../components/Loading'
+import NoData from '../components/NoData'
+import Axios from '../utils/Axios'
+import SummaryApi from '../common/SummaryApi'
+import EditCategory from '../components/EditCategory'
+import CofirmBox from '../components/CofirmBox'
+import toast from 'react-hot-toast'
+import AxiosToastError from '../utils/AxiosToastError'
+import { useSelector } from 'react-redux'
 
-const CategoryPage = () =>{
+const CategoryPage = () => {
     const [openUploadCategory,setOpenUploadCategory] = useState(false)
     const [loading,setLoading] = useState(false)
     const [categoryData,setCategoryData] = useState([])
@@ -13,7 +23,13 @@ const CategoryPage = () =>{
     const [deleteCategory,setDeleteCategory] = useState({
         _id : ""
     })
+    // const allCategory = useSelector(state => state.product.allCategory)
 
+
+    // useEffect(()=>{
+    //     setCategoryData(allCategory)
+    // },[allCategory])
+    
     const fetchCategory = async()=>{
         try {
             setLoading(true)
@@ -55,8 +71,7 @@ const CategoryPage = () =>{
         }
     }
 
-
-    return (
+  return (
     <section className=''>
         <div className='p-2   bg-white shadow-md flex items-center justify-between'>
             <h2 className='font-semibold'>Category</h2>
@@ -68,14 +83,61 @@ const CategoryPage = () =>{
             )
         }
 
+        <div className='p-4 grid  grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2'>
+            {
+                categoryData.map((category,index)=>{
+                    return(
+                        <div className='w-32 h-56 rounded shadow-md' key={category._id}>
+                            <img 
+                                alt={category.name}
+                                src={category.image}
+                                className='w-full object-scale-down'
+                            />
+                            <div className='items-center h-9 flex gap-2'>
+                                <button onClick={()=>{
+                                    setOpenEdit(true)
+                                    setEditData(category)
+                                }} className='flex-1 bg-green-100 hover:bg-green-200 text-green-600 font-medium py-1 rounded'>
+                                    Edit
+                                </button>
+                                <button onClick={()=>{
+                                    setOpenConfirmBoxDelete(true)
+                                    setDeleteCategory(category)
+                                }} className='flex-1 bg-red-100 hover:bg-red-200 text-red-600 font-medium py-1 rounded'>
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
 
-         {
+        {
+            loading && (
+                <Loading/>
+            )
+        }
+
+        {
             openUploadCategory && (
                 <UploadCategoryModel fetchData={fetchCategory} close={()=>setOpenUploadCategory(false)}/>
             )
         }
 
+        {
+            openEdit && (
+                <EditCategory data={editData} close={()=>setOpenEdit(false)} fetchData={fetchCategory}/>
+            )
+        }
+
+        {
+           openConfimBoxDelete && (
+            <CofirmBox close={()=>setOpenConfirmBoxDelete(false)} cancel={()=>setOpenConfirmBoxDelete(false)} confirm={handleDeleteCategory}/>
+           ) 
+        }
     </section>
-    );
+  )
 }
+
 export default CategoryPage
