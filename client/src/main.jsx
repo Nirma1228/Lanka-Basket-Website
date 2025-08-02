4e86b42c78f615294cb0b253741f6e26df317660
Chanuka <herathh-im22057@stu.kln.ac.lk>
2025-08-02 21:05:23 +0530

    updated main .jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './route/index'
+import router from './route/index.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 
