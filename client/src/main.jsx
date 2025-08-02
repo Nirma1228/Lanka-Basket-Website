a55c8fcfc4476d3140dfbab687d0fc4dd843340e
Chanuka <herathh-im22057@stu.kln.ac.lk>
2025-08-02 21:13:27 +0530

    Changed case sensitive data

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './route/index.jsx'
+import router from './route/Index.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 
