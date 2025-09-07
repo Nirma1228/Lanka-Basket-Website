f3be846452bd2128b2bb9f274d88ff9fb7900db0
Herath H.M.M.C. <herathh-im22057@stu.kln.ac.lk>
2025-09-07 13:18:27 +0530

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 1000,
+     outDir: 'build'
   }
  // server: {
  //   port: 5177, 
